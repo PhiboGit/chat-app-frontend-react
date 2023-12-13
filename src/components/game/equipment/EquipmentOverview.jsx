961b1823d1e23e05a349fe79a78af7cd7afc2efe
Phibo <phibo96@gmail.com>
2023-12-13 14:50:40 +0100

    unequip added

--- src/components/game/equipment/EquipmentOverview.jsx
+++ src/components/game/equipment/EquipmentOverview.jsx
@@ -4,7 +4,6 @@ import { CharacterDataContext } from '../dataProviders/CharacterDataProvider';
 
 import ExpBar from '../ExpBar';
 
-import CarpenterIcon from '@mui/icons-material/Carpenter';
 
 import Switch from '@mui/material/Switch';
 import TextField from '@mui/material/TextField';
@@ -28,15 +27,23 @@ import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import { Key } from '@mui/icons-material';
 
-const Item = styled(Paper)(({ theme }) => ({
+import CarpenterIcon from '@mui/icons-material/Carpenter';
+import HelpCenterIcon from '@mui/icons-material/HelpCenter';
+
+const Item = styled(Paper)(({ theme, icon }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
   ...theme.typography.body2,
   padding: theme.spacing(1),
   textAlign: 'center',
   width: 60,
   height: 60,
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
   color: theme.palette.text.secondary,
 }));
+
 
 const EquipmentOverview = () => {
   const { gameData, send } = useContext(GameDataContext);
@@ -48,7 +55,7 @@ const EquipmentOverview = () => {
 
   const [profession, setProfession] = React.useState("");
   const [equipmentSlot, setEquipmentSlot] = React.useState("")
-  const [itemId, setItemId] = useState("")
+  const [itemId, setItemId] = useState("null")
 
   const idToItemMap = characterData.items.reduce((map, item) => {
     map[item._id] = item;
@@ -58,12 +65,13 @@ const EquipmentOverview = () => {
   function equipItem(profession, slot){
     setProfession(profession)
     setEquipmentSlot(slot)
+    setItemId(skills[profession].equipment[slot] ? skills[profession].equipment[slot] : "null" )
     setOpen(true);
   }
 
   const handleClose = () => {
     setOpen(false);
-    setItemId("")
+    setItemId("null")
   };
 
   const handleItem = (event) => {
@@ -84,6 +92,7 @@ const EquipmentOverview = () => {
       }
     }
     send(equip)
+    setItemId("null")
     setOpen(false)
   };
   
@@ -98,7 +107,12 @@ const EquipmentOverview = () => {
           <Grid key={profession} container spacing={1}>
             {Object.keys(skills[profession].equipment).map((slot) => (
               <Grid key={slot} item >
-                <Item onClick={() => equipItem(profession, slot)}>{`${slot}`}</Item>
+                <Item 
+                
+                onClick={() => equipItem(profession, slot)}
+                >
+                  <HelpCenterIcon sx={{ fontSize: 90 }}/>
+                </Item>
               </Grid>
             ))}
           </Grid>
@@ -124,6 +138,9 @@ const EquipmentOverview = () => {
               label="Item"
               onChange={handleItem}
             >
+              <MenuItem key="null" value="null">
+                "null"
+              </MenuItem>
               {Object.keys(idToItemMap).map((key) => (
                 <MenuItem key={key} value={key}>
                   {key}
