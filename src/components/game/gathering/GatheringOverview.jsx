3635c573eb962d20d70f4420ba876ac2c6fe1e53
Phibo <phibo96@gmail.com>
2023-12-15 16:31:53 +0100

    removed icons

--- src/components/game/gathering/GatheringOverview.jsx
+++ src/components/game/gathering/GatheringOverview.jsx
@@ -3,16 +3,8 @@ import { GameDataContext } from '../dataProviders/GameDataProvider';
 import { CharacterDataContext } from '../dataProviders/CharacterDataProvider';
 import ExpBar from '../ExpBar';
 
-import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
-
-import CarpenterIcon from '@mui/icons-material/Carpenter';
-import AllInclusiveIcon from '@mui/icons-material/AllInclusive';
-import GrassIcon from '@mui/icons-material/Grass';
-import ForestIcon from '@mui/icons-material/Forest';
-import TerrainIcon from '@mui/icons-material/Terrain';
 
 import Switch from '@mui/material/Switch';
 import TextField from '@mui/material/TextField';
@@ -20,47 +12,6 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import GatheringIcon from './GatheringIcon';
 
-
-const Item = ({ icon: IconComponent, selected, onClick  }) => {
-  const [backgroundColor, setBackgroundColor] = useState('white');
-
-  const paperStyle = {
-    width: 60,
-    height: 60,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    transition: 'background-color 0.3s ease',
-    cursor: 'pointer',
-    backgroundColor: selected ? 'lightgreen' : backgroundColor,
-  };
-
-  const iconStyle = {
-    width: '100%',
-    height: '100%',
-  };
-
-  const handleHover = () => {
-    setBackgroundColor('lightgray');
-  };
-
-  const handleLeave = () => {
-    setBackgroundColor('white');
-  };
-
-  return (
-    <Paper
-      style={paperStyle}
-      onMouseEnter={handleHover}
-      onMouseLeave={handleLeave}
-      onClick={onClick}
-    >
-      <div style={iconStyle}>
-        {IconComponent && <IconComponent style={{ width: '100%', height: '100%' }} />}
-      </div>
-    </Paper>
-  );
-};
 
 const GatheringOverview = () => {
   const { gameData, send } = useContext(GameDataContext);
@@ -74,35 +25,6 @@ const GatheringOverview = () => {
   
 
   const gatheringData = gameData.gatheringResourcesData[profession].tiers[tier-1]
-  
-  function getGatheringData(profession, tier){
-    const tierData = gatheringData[tier]
-    const exp = tierData.exp
-    const expChar = tierData.expChar
-    const level = tierData.level
-    const time = tierData.time
-    const amountMin = tierData.amountMin
-    const amountMax = tierData.amountMax
-    
-    return {exp, expChar, level, time, amountMin, amountMax}
-  }
-  
-  const getIcon = (profession) => {
-    switch (profession) {
-      case 'mining': return TerrainIcon
-      case 'woodcutting': return ForestIcon
-      case 'harvesting': return GrassIcon
-      default:
-        return CarpenterIcon
-    }
-  }
-
-  const handleItemClick = (itemIndex, profession, tier) => {
-    console.log(`selectGathering: ${itemIndex}`);
-    setSelectedItem(itemIndex);
-    setProfession(profession)
-    setTier(tier)
-  };
 
   const handleLimit = (event) => {
     setLimit(event.target.checked);
