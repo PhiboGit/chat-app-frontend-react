c44ebb43709436fe138162c8dfc698f84f9e9c04
Phibo <phibo96@gmail.com>
2024-03-22 22:51:30 +0100

    a better context with a partial store

---
+++ src/components/game/dataProviders/createPartialContextStore.jsx
@@ -0,0 +1,73 @@
+// https://github.com/jherr/fast-react-context
+
+import React, {
+  useRef,
+  createContext,
+  useContext,
+  useCallback,
+  useSyncExternalStore,
+} from "react";
+
+export default function createPartialContextStore(initialState) {
+  function useStoreData(){
+    // holds any type of data in the store
+    const store = useRef(initialState);
+
+    // lets you access the stored data
+    const get = useCallback(() => store.current, []);
+
+    // functions that need to be called when the store is updated
+    const subscribers = useRef(new Set());
+
+    // lets you change the state of the store
+    const set = useCallback((value) => {
+      // everything stays unchanged except for the specfied value
+      store.current = { ...store.current, ...value };
+      // notify the subscribers that the store has changed
+      subscribers.current.forEach((callback) => callback());
+    }, []);
+
+    // add a function to the subscribers; return a function to unsub
+    const subscribe = useCallback((callback) => {
+      subscribers.current.add(callback);
+      return () => subscribers.current.delete(callback);
+    }, []);
+
+    return {
+      get,
+      set,
+      subscribe,
+    };
+  }
+
+  const StoreContext = createContext(null);
+
+  function Provider({ children }) {
+    return (
+      <StoreContext.Provider value={useStoreData()}>
+        {children}
+      </StoreContext.Provider>
+    );
+  }
+
+  //custom hook for the store, the selector lets you listen to only partial data of the state
+  function useStore(selector) {
+    const store = useContext(StoreContext);
+    if (!store) {
+      throw new Error("Store not found");
+    }
+
+    // trigger a re-render when the selected state changes
+    const state = useSyncExternalStore(
+      store.subscribe,
+      () => selector(store.get())
+    );
+
+    return [state, store.set];
+  }
+
+  return {
+    Provider,
+    useStore,
+  };
+}
