3a053129256d3459cf968d468213fa8cd865bc33
Phibo <phibo96@gmail.com>
2024-03-24 18:12:57 +0100

    updating char correctly

--- src/components/game/dataProviders/CharacterProvider.jsx
+++ src/components/game/dataProviders/CharacterProvider.jsx
@@ -8,7 +8,8 @@ let useCharacterStore
 
 // Updater is a child of Provider to access the store
 const CharacterProvider = ({children, initChar}) => {
-  const { Provider, useStore } = createPartialContextStore(initChar.character);
+  // deep copy, cause currently there are two characterUpdater with the same initial state initChar
+  const { Provider, useStore } = createPartialContextStore(structuredClone(initChar.character));
   useCharacterStore = useStore
   return (
     <Provider>
@@ -19,12 +20,19 @@ const CharacterProvider = ({children, initChar}) => {
   )
 }
 
+const resolvePath = (object, path, defaultValue) => path
+  .split('.')
+  .reduce((o, p) => o ? o[p] : defaultValue, object)
+
+const setPath = (object, path, updater, defaultValue) => path
+  .split('.')
+  .reduce((o,key,i) => o[key] = path.split('.').length === ++i ? updater(o[key] !== undefined ? o[key] : defaultValue) : o[key] || {}, object)
+
 // subs to the event emitted by the websocket
 const Updater = ({children, useStore}) => {
   const [charState, setState] = useStore((char) => char);
   
   useEffect(() => {
-    //setState(initChar.character)
     messageReceiver.addEventListener('update_char' , updateChar)
     return () => {
       messageReceiver.removeEventListener('update_char', updateChar)
@@ -32,73 +40,42 @@ const Updater = ({children, useStore}) => {
   }, [])
   
   function updateChar(event) {
-    const receivedData = event.detail;  
+    const updates = event.detail;  
     const updateOperations = ["$inc", "$set", "$push", "$pull"];
   
     setState( prev => {
       const char = {...prev}
+      let updatedPaths = {}
       updateOperations.forEach((operation) => {
-        if (receivedData.hasOwnProperty(operation)) {
-          Object.entries(receivedData[operation]).forEach(([key, value]) => {
-            console.log("update: ", operation, key, value);
-            changeValue(char, key, value, operation);
+        if (updates.hasOwnProperty(operation)) {
+          Object.entries(updates[operation]).forEach(([path, value]) => {
+            console.log("update: ", operation, path, value);
+            const newValue = changeValue(char, path, value, operation);
+            const changedPath = {}
+            setPath(changedPath, path,() => newValue)
+            updatedPaths = {...updatedPaths, ...changedPath}
           });
         }
       });
-      return char
+      console.log("character paths updated:", updatedPaths)
+      return updatedPaths
     })
-    console.log("characterData updated!", charState)
   }
 
-  function changeValue(char, key, value, operation) {
-    // traverse through the char data by key
-    const keys = key.split('.');
-    // tree start, root, pointer to the entry that holds the key,value pair that needs to be changed
-    let currentObj = char;
-    // find the entry of the charData that need change based on the key. It is still and object.
-    // we stop at the object that holds the key/value. (keys[keys.length - 2])
-    for (let i = 0; i < keys.length - 1; i++) {
-      const currentKey = keys[i];
-      if (!currentObj[currentKey]) {
-        console.error("Invalid key for key: ", currentKey);
-        return;
-      }
-      // set the pointer to the current key
-      currentObj = currentObj[currentKey];
-    }
-    // currentObj now points to the object that should have the entry that needs to be updated
-  
-    // the entry that needs to be updated
-    const lastKey = keys[keys.length - 1];
 
+  function changeValue(char, path, value, operation) {
     switch (operation) {
-      case "$inc":
-        currentObj[lastKey] += value;
-        break;
       case "$set":
-        currentObj[lastKey] = value;
+        return setPath(char, path, (prev) => value)
+        break;
+      case "$inc":
+        return setPath(char, path, (prev) => prev + value)
         break;
       case "$push":
-        if(lastKey == 'items' && typeof value === 'string'){
-          return
-          // if it is an item update, the item is wrapped in an array
-        } else if(lastKey == 'items' && Array.isArray(value)){
-          for(const newItem of value){
-            // overwrite existing item
-            if(idToItemMap[newItem._id]){
-              Object.assign(item, newItem);
-            } else {
-              // or just add the item
-              currentObj[lastKey].push(newItem);
-            }
-          }
-          currentObj[lastKey]
-        }else {
-          currentObj[lastKey].push(value);
-        }
+        return setPath(char, path, (prev) => prev.push(value))
         break;
       case "$pull":
-        currentObj[lastKey] = currentObj[lastKey].filter(item => item._id != value);
+        return setPath(char, path, (prev) => prev.filter(item => item._id != value))
         break;
       default:
         console.error("Invalid operation: ", operation);
