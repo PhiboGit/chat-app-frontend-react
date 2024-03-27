2980b0ee3302af41d9e8696f06780209065872ba
Phibo <phibo96@gmail.com>
2024-03-27 01:40:36 +0100

    order of providers

--- src/components/game/dataProviders/DataProviders.jsx
+++ src/components/game/dataProviders/DataProviders.jsx
@@ -2,15 +2,18 @@ import React, {
 } from "react";
 import { TestChatProvider } from "./TestChatProvider";
 import { CharacterProvider } from "./CharacterProvider";
+import { ItemProvider } from "./ItemProvider";
 
 // should hold every DataProvider for better overview
 export const DataProviders = ({children, initChar}) =>{
 
   return (
     <TestChatProvider initChar={initChar}>
-      <CharacterProvider initChar={initChar}>
-        {children}
-      </CharacterProvider>
+      <ItemProvider initChar={initChar}>
+        <CharacterProvider initChar={initChar}>
+          {children}
+        </CharacterProvider>
+      </ItemProvider>
     </TestChatProvider>
   )
 } 
